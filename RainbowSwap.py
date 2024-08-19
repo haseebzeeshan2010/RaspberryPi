b1f12b6ddd6f719f0e2b098760b963766448f4c9
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-08-19 16:56:32 +0100

    Change individual pixel colours randomly

--- RainbowSwap.py
+++ RainbowSwap.py
@@ -1,5 +1,12 @@
 from sense_hat import SenseHat
-
+from random import randint
+from time import sleep
 
 sense = SenseHat()
-sense.clear((255,255,255))
+
+while True:
+        
+    for i in range(0,8):
+        for j in range(0,8):
+            sense.set_pixel(i,j,(randint(0,150),randint(0,150),randint(0,150)))
+            sleep(0.01)
