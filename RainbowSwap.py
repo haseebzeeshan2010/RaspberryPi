df20d3983294ffb51ae05942d537dc637084f92e
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-08-20 21:04:55 +0100

    Check joystick direction

--- RainbowSwap.py
+++ RainbowSwap.py
@@ -8,11 +8,14 @@ while True:
         
     for i in range(0,8):
         for j in range(0,8):
-            sense.set_pixel(i,j,(randint(0,150),randint(0,150),randint(0,150),),)
+            sense.set_pixel(i,j,(randint(0,150),randint(0,150),randint(0,150)))
             sleep(0.01)
             
     for i in range(0,8):
         for j in range(0,8):
             sense.set_pixel(i,j,(0,0,0))
             sleep(0.01)
+
+    for event in sense.stick.get_events():
+        print(event.direction, event.action)
         
