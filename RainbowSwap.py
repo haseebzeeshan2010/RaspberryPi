2edea9aa4aedb10bfebd08b2311c1bb27d7af11c
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-08-22 21:09:17 +0100

    Take mean of values

--- RainbowSwap.py
+++ RainbowSwap.py
@@ -10,6 +10,16 @@ pressure = sense.get_pressure()
 temperature = sense.get_temperature()
 humidity = sense.get_humidity()
 
+for i in range(0,20):
+    pressure += sense.get_pressure()
+    humidity += sense.get_humidity()
+    sleep(1)
+    print(f"press: {pressure}  humid: {humidity}")
+
+pressure = pressure / 21
+humidity = humidity / 21
+
+
 print(f"temperature: {temperature}" )
 print(f"humidity: {humidity}")
 print(f"pressure: {pressure} ")
