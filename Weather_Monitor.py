f6aa8394c09f51dddb49e5d161fa21119271ad34
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-08-25 13:09:04 +0100

    Create new more accurate data logger

--- Weather_Monitor.py
+++ Weather_Monitor.py
@@ -1,38 +1,68 @@
+# from sense_hat import SenseHat
+# from random import randint
+# from time import sleep
+# from datetime import datetime
+# import csv
+
+# sense = SenseHat()
+# sense.clear()
+
+# while True:
+
+# 	pressure = sense.get_pressure()
+# 	temperature = sense.get_temperature()
+# 	humidity = sense.get_humidity()
+
+
+# 	for i in range(0,20):
+# 		pressure += sense.get_pressure()
+# 		humidity += sense.get_humidity()
+# 		sleep(5)
+# 		#sleep(1)
+
+# 	pressure = pressure / 21
+# 	humidity = humidity / 21
+
+# 	print(f"temperature: {temperature}" )
+# 	print(f"humidity: {humidity}")
+# 	print(f"pressure: {pressure} ")
+# 	print(f"date/time: {datetime.now()}")
+
+# 	with open('Weather_Data.csv', mode='a') as weather_file:
+# 		weather_writer = csv.writer(weather_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+# 		weather_writer.writerow([f"{datetime.now()}",f"{humidity}",f"{pressure}" "0"])
+# 	sleep(1695)
+	
+
+
 from sense_hat import SenseHat
-from random import randint
-from time import sleep
+import time
 from datetime import datetime
 import csv
-
 sense = SenseHat()
 sense.clear()
 
 while True:
-
-	pressure = sense.get_pressure()
-	temperature = sense.get_temperature()
-	humidity = sense.get_humidity()
-
-
-	for i in range(0,20):
-		pressure += sense.get_pressure()
-		humidity += sense.get_humidity()
-		sleep(5)
-		#sleep(1)
-
-	pressure = pressure / 21
-	humidity = humidity / 21
-
-	print(f"temperature: {temperature}" )
-	print(f"humidity: {humidity}")
-	print(f"pressure: {pressure} ")
-	print(f"date/time: {datetime.now()}")
-
-	with open('Weather_Data.csv', mode='a') as weather_file:
-		weather_writer = csv.writer(weather_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		weather_writer.writerow([f"{datetime.now()}",f"{humidity}",f"{pressure}" "0"])
-	sleep(1695)
-	
+	t = time.localtime()
+	if t.tm_min in [0,15,30,45]:
+		print("YES")
+		current_time = datetime.now()
+		
+		pressure = sense.get_pressure()
+		temperature = sense.get_temperature_from_pressure()
+		humidity = sense.get_humidity()
 
 
+		for i in range(0,20):
+			pressure += sense.get_pressure()
+			humidity += sense.get_humidity()
+			temperature += sense.get_temperature_from_pressure()
+			time.sleep(5)
+			#sleep(1)
 
+		pressure = pressure / 21
+		humidity = humidity / 21
+		temperature = temperature / 21
+		with open('Weather_Data.csv', mode='a') as weather_file:
+			weather_writer = csv.writer(weather_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+			weather_writer.writerow([f"{current_time}",f"{humidity}",f"{pressure}", f"{temperature}"])
