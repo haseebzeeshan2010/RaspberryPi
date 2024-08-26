8b6268414968b2a5d9b072fea29ad8c59fcf4117
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-08-26 15:57:30 +0100

    Take new readings and add visual graphs for data

--- Weather_Analyser.py
+++ Weather_Analyser.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('Weather_Data.csv')
-print(df['Humidity'][:8])
+print(df['Humidity'])
 print(df['Pressure'][:8])
+
+fig, ax = plt.subplots()             # Create a figure containing a single Axes.
+ax.plot(df['Date/Time'], df['Pressure'])  # Plot some data on the Axes.
+plt.show()
