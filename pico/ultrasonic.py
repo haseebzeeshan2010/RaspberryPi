54044438274958598f776fd2e57080106b52b495
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-09-01 21:48:14 +0100

    Detect temperature with inbuilt temperature sensor

--- pico/ultrasonic.py
+++ pico/ultrasonic.py
@@ -82,4 +82,4 @@ sensor = HCSR04(trigger_pin=2, echo_pin=3, echo_timeout_us=10000)
 while True:
     distance = sensor.distance_cm()
     print('Distance:', distance, 'cm')
-    sleep(0.5)
+    sleep(1)
