a62d284a56d90a8bf7e3bb746a2a103742f030ab
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-09-08 18:19:50 +0100

    Add LED feedback

--- pico/ultrasonic.py
+++ pico/ultrasonic.py
@@ -4,6 +4,7 @@ from picozero import Speaker
 trigger = Pin(3, Pin.OUT)
 echo = Pin(2, Pin.IN)
 speaker = Speaker(15)
+myLED = Pin(14,Pin.OUT)
 def ultra():
    trigger.low()
    utime.sleep_us(2)
@@ -18,10 +19,18 @@ def ultra():
    distance = (timepassed * 0.0343) / 2
    print("The distance from object is ",distance,"cm")
    
-   if distance < 65:
+   if distance < 62:
        print("TOO CLOSE")
+       myLED.value(1)
+       speaker.play('c7', 0.01)
+       utime.sleep(0.01)
+       speaker.play('c6', 0.01)
+       utime.sleep(0.01)
+       speaker.play('c5', 0.01)
+       utime.sleep(0.01)
        speaker.play('c4', 0.01)
-   
+       utime.sleep(1)
+       myLED.value(0)
 while True:
    ultra()
    utime.sleep(2)
