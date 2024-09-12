b095a82dfd1388ad3d5861f9935a8d0305cb81bf
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2024-09-12 19:34:10 +0100

    Simplify ultrasonic sensor code

--- pico/ultrasonic.py
+++ pico/ultrasonic.py
@@ -1,9 +1,9 @@
 from machine import Pin
 import utime
-from picozero import Speaker
+
 trigger = Pin(3, Pin.OUT)
 echo = Pin(2, Pin.IN)
-speaker = Speaker(15)
+
 myLED = Pin(14,Pin.OUT)
 def ultra():
    trigger.low()
@@ -18,19 +18,7 @@ def ultra():
    timepassed = signalon - signaloff
    distance = (timepassed * 0.0343) / 2
    print("The distance from object is ",distance,"cm")
-   
-   if distance < 62:
-       print("TOO CLOSE")
-       myLED.value(1)
-       speaker.play('c7', 0.01)
-       utime.sleep(0.01)
-       speaker.play('c6', 0.01)
-       utime.sleep(0.01)
-       speaker.play('c5', 0.01)
-       utime.sleep(0.01)
-       speaker.play('c4', 0.01)
-       utime.sleep(1)
-       myLED.value(0)
+
 while True:
    ultra()
    utime.sleep(2)
